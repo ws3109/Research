65c7b771ca058fdb97e1153cbf5142896282e4a9
Wenbo <ws3109@g.rit.edu>
2017-10-18 21:13:53 -0400

    Update plotTwoMatchings.py

--- sample perfect matchings/plotTwoMatchings.py
+++ sample perfect matchings/plotTwoMatchings.py
@@ -1,20 +1,24 @@
 import matplotlib.pyplot as plt
 
-def plot(size, lines1, lines2, crossings, save_image = False, image_folder = None, image_name = None):
-    lines1, lines2 = set(lines1), set(lines2)
+def plotEdges(edges, color):
+    for p, q in edges:
+        plt.plot([p[0], q[0]], [p[1], q[1]], color, linewidth = 1)
+
+def plotLattice(size):
     x, y = zip(*[(x, y) for x in range(size) for y in range(size)])
-    plt.plot([x for x in range(size)], [y for _ in range(size)], 'ko', markersize = 2)
-    if len(crossings) > 0:
-        cx, cy = zip(*crossings)
-        plt.plot(cx, cy, 'o', markersize = 10, color = 'k', mfc ='none', markeredgewidth= 1)
-    for p, q in lines1.intersection(lines2):
-        plt.plot([p[0], q[0]], [p[1], q[1]], 'g')
-    for p, q in lines1.symmetric_difference(lines2):
-        if (p, q) in lines1:
-            plt.plot([p[0], q[0]], [p[1], q[1]], 'b')
-        else:
-            plt.plot([p[0], q[0]], [p[1], q[1]], 'r')
+    plt.plot([x for x in range(size)], [y for _ in range(size)], 'ko', markersize = 1)
     plt.axis([-2, size + 2, -2, size + 2])
+
+def plotTwoMatchings(size, lines1, lines2, crossings, save_image = False, image_folder = None, image_name = None):
+    plotLattice(size)
+    # plot crossings
+    cx, cy = zip(*crossings)
+    plt.plot(cx, cy, 'o', markersize=10, color='k', mfc='none', markeredgewidth=1)
+    # plot two matchings
+    lines1, lines2 = set(lines1), set(lines2)
+    plotEdges(lines1.intersection(lines2), 'g')
+    plotEdges(lines1 - lines2, 'r')
+    plotEdges(lines2 - lines1, 'b')
     plt.title('There are %d crossings.' % (len(crossings)))
     if save_image:
         plt.savefig(image_folder + image_name)
