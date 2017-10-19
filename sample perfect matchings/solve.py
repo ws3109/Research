56a8423e7ad1d1380d7a3fbdc647e5588cb6255e
Wenbo <ws3109@g.rit.edu>
2017-10-18 21:13:10 -0400

    Update solve.py

--- sample perfect matchings/solve.py
+++ sample perfect matchings/solve.py
@@ -5,10 +5,11 @@ import itertools
 from os import listdir
 from os.path import isfile, join
 from MatchingOnLattice import *
-from plotTwoMatchings import *
+from plotFunctions import *
 
 moves = [(-1, 0), (1, 0), (0, 1), (0, -1)]
 move_func = lambda x, y: (x[0] + y[0], x[1] + y[1])
+
 def findCrossings(init_edges_list, final_edges_dict):
     count, crossings = 0, []
     valid_init_edges = [(p, q) for p, q in init_edges_list if isUnitDiagonal(p, q)]
@@ -21,9 +22,8 @@ def findCrossings(init_edges_list, final_edges_dict):
     return set(crossings)
 
 def generateMatchings(size, mixingTime, numSamples, horizontal = True):
-    print('Sampling matchings on lattice of size %d.' % (size))
-    m = MatchingOnLattice(size, size, horizontal)
-    return m.getSamples(mixingTime = mixingTime, numSamples = numSamples)
+    print('Generating matchings on lattice of size %d start from %s matching' % (size, 'horizontal' if horizontal else 'vertical'))
+    return MatchingOnLattice(size, size, horizontal).getSamples(mixingTime = mixingTime, numSamples = numSamples)
 
 def combineInitAndFinal(size, init_samples, final_samples, init_final_path_and_file):
     print('Generating pairs of initial and final matching on lattice of size %d.' % (size))
@@ -53,28 +53,29 @@ def analyze(pair_folder, stat_folder):
     with open(stat_folder + 'data.txt', 'w') as f:
         f.write('\n'.join(map(str, zip(sizes, num_crossings, ratios))))
 
-def plotPairs(pair_folder, image_folder, save_image = False):
-    files = sorted([f for f in listdir(pair_folder) if isfile(join(pair_folder, f))])
-    max_num_crossing = {}
-    for file in files:
-        with open(pair_folder + file, 'rb') as handle:
-            size, crossing_list = pickle.load(handle)
-        initials, finals, crossings = zip(*crossing_list)
-        max_num_crossing[size] = max([len(x) for x in crossings])
-        if save_image:
-            max_pairs_indices = [i for i in range(len(crossings)) if len(crossings[i]) == max_num_crossing[size]]
-            image_count = 0
-            for i in max_pairs_indices:
+def plotPairs(pair_file, image_folder, save_image = False):
+    max_num_crossing, exist = {}, {}
+    with open(pair_file, 'rb') as handle:
+        size, crossing_list = pickle.load(handle)
+    initials, finals, crossings = zip(*crossing_list)
+    max_num_crossing[size] = max([len(x) for x in crossings])
+    if save_image:
+        max_pairs_indices = [i for i in range(len(crossings)) if len(crossings[i]) == max_num_crossing[size]]
+        image_count = 0
+        for i in max_pairs_indices:
+            init_key, final_key = tuple(sorted(initials[i])), tuple(sorted(finals[i]))
+            if init_key not in exist and final_key not in exist:
+                exist[init_key], exist[final_key] = True, True
+                plotTwoMatchings(size, initials[i], finals[i], crossings[i], save_image, image_folder, str(image_count)+'.png')
                 image_count += 1
-                plot(size, initials[i], finals[i], crossings[i], save_image, image_folder, str(image_count)+'.png')
 
 def create_folder(folder):
     if not os.path.exists(folder):
         os.makedirs(folder)
 
 def main():
-    mixingTime = [i for i in range(10000, 10001, 1000)]
-    numSamples = [i for i in range(400, 401, 100)]
+    mixingTime = [i for i in range(1000, 1001, 1000)]
+    numSamples = [i for i in range(100, 101, 100)]
     sub_path = 'mixingTime-%d-numOfSamples-%d/'
     for t, num in itertools.product(mixingTime, numSamples):
         pair_folder = 'pairs/' + sub_path % (t, num)
@@ -83,19 +84,17 @@ def main():
         create_folder(image_folder)
         stat_folder = 'stat/' + sub_path % (t, num)
         create_folder(stat_folder)
-    for t, num in itertools.product(mixingTime, numSamples):
-        for size in range(4, 41, 2):
+        for size in range(4, 7, 2):
             # generate pairs
             samples_horizontal = generateMatchings(size, t, num, horizontal=True)
             samples_vertical = generateMatchings(size, t, num, horizontal=False)
+            # # put them together
             pair_file = '%d.pickle' % (size)
             combineInitAndFinal(size, samples_horizontal, samples_vertical, pair_folder + pair_file)
-
             # generate images
-            image_subfolder = image_folder + str(size) + '/'
-            create_folder(image_subfolder)
-            plotPairs(pair_folder, image_subfolder, save_image = True)
-
+            image_subfoler = image_folder + str(size) + '/'
+            create_folder(image_subfoler)
+            plotPairs(pair_folder + pair_file, image_subfoler, save_image = True)
             # analyze the result
             analyze(pair_folder, stat_folder)
 
